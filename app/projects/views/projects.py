09daf731ea2e217da35a0e25bd5ac64d14409170
quamilek <jolka@MacBook-Pro-jolka.local>
2024-02-24 23:25:04 +0100

    login form update template

--- app/projects/views/projects.py
+++ app/projects/views/projects.py
@@ -2,11 +2,15 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from projects.models import Project
+from projects.models import Project, ProjectElement
 
 
-class ProjectsView(View):
+
+class ProjectsView(LoginRequiredMixin, View):
     template_name = "projects.html"
 
     def get(self, request, *args, **kwargs):
@@ -19,16 +23,31 @@ class ProjectsView(View):
         )
 
 
-class ProjectView(View):
+class ProjectView(LoginRequiredMixin, View):
     template_name = "project.html"
+
+    @login_required
+    def dispatch(self, request, *args, **kwargs):
+        # Your view logic here
+        pass
 
     def get(self, request, *args, **kwargs):
 
         project_id = kwargs.get('id')
         project = get_object_or_404(Project, pk=project_id)
+        project_elements = ProjectElement.objects.filter(project=project)
+
+        paginator = Paginator(project_elements, 10) # 10 elements per page
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
 
         return render(
             request, 
             self.template_name, 
-            {"project": project}
+            {
+                "project": project,
+                "page_obj": page_obj,
+            }
         )
